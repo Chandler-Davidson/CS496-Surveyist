e712c7da53614a9bf60f6f179c2d1a7f23f390fc
Chandler Davidson <chandlermdavidson@gmail.com>
2019-04-03 10:10:12 -0500

    Add global styling

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import Head from 'next/head';
 
 import Header from './header';
 
 export default function Page({ children }) {
   return (
     <div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <Header />
       {children}
+      <style jsx global>{`
+        body {
+          background: #f8f9fc;
+          font: 11px menlo;
+          margin: 1%;
+        }
+      `}</style>
     </div>
   );
 }
