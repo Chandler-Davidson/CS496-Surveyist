b1817c1cfa33f237492e5da1bd2bd7f14a7395b2
Chandler Davidson <chandlermdavidson@gmail.com>
2019-04-15 11:56:57 -0500

    Seperate db and endpoints

--- Server/SurveyistServer/DatabaseManager.cs
+++ Server/SurveyistServer/DatabaseManager.cs
@@ -1,77 +1,55 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.IO;
+﻿using System.Linq;
+using MongoDB.Bson;
 using MongoDB.Driver;
-using Nancy;
-using Nancy.Responses;
 
 namespace SurveyistServer
 {
-    public class DatabaseManager : NancyModule
+    public class DatabaseManager
     {
         private readonly FilterDefinition<BsonDocument> _emptyFilter = Builders<BsonDocument>.Filter.Empty;
-
-        public DatabaseManager()
-        {
-            Options["/{catchAll*}"] = parmeters => { return new Response {StatusCode = HttpStatusCode.Accepted}; };
-
-            After.AddItemToEndOfPipeline(context =>
-            {
-                context.Response.WithHeader("Access-Control-Allow-Origin", "*")
-                    .WithHeader("Access-Control-Allow-Methods", "POST, GET")
-                    .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
-            });
-
-            Get[nameof(PreviousSurveys)] = _ => PreviousSurveys();
-            Get[nameof(ChartTypes)] = _ => ChartTypes();
-            //Get[$"{nameof(PreviousRun)}/{{runId}}"] = parameters => PreviousRun(parameters.runId);
-            //Get[nameof(RuleDefinitions)] = _ => RuleDefinitions();
-            Post[nameof(NewSurvey), true] = async (parameters, token) => NewSurvey();
-        }
-
         internal static MongoClient Client { get; } = new MongoClient("mongodb://localhost:27017");
         internal static IMongoDatabase Database { get; } = Client.GetDatabase("Surveyist");
 
-        internal static Response GetResponse(string collectionName, FilterDefinition<BsonDocument> filter)
+        internal IMongoCollection<BsonDocument> GetCollection(string collectionName)
         {
-            var collection = Database.GetCollection<BsonDocument>(collectionName);
+            // Wrapper to fetch collection
+            return Database.GetCollection<BsonDocument>(collectionName);
+        }
 
+        internal IAsyncCursor<BsonDocument> GetDocuments(string collectionName,
+            FilterDefinition<BsonDocument> filter = null)
+        {
+            // Get collection
+            var collection = GetCollection(collectionName);
+
+            // Apply filter if not null, otherwise get all
+            filter = filter ?? _emptyFilter;
+
+            // Return docs
+            return collection.FindSync<BsonDocument>(filter);
+        }
+
+        internal void InsertNewDocument(string collectionName, string contents)
+        {
+            // Create a new document
+            var newDoc = BsonDocument.Parse(contents);
+
+            // Fetch collection
+            var collection = GetCollection(collectionName);
+
+            // Insert document
+            collection.InsertOne(newDoc);
+        }
+
+        /// <summary>
+        ///     Returns true if a document matches the given filter.
+        /// </summary>
+        internal bool ExistsInCollection(IMongoCollection<BsonDocument> collection,
+            FilterDefinition<BsonDocument> filter)
+        {
             var docs = collection.FindSync<BsonDocument>(filter);
 
-            return docs.AsResponse();
-        }
-
-        private Response PreviousSurveys()
-        {
-            return GetResponse("PreviousSurveys", _emptyFilter);
-        }
-
-        private Response ChartTypes()
-        {
-            return GetResponse("ChartTypes", _emptyFilter);
-        }
-
-        private Response NewSurvey()
-        {
-            var surveyConfig = Request.Form;
-
-            var newDoc = BsonDocument.Parse(surveyConfig);
-            var SurveyDetails = Database.GetCollection<BsonDocument>("SurveyDetails");
-            SurveyDetails.InsertOne(newDoc);
-
-            return HttpStatusCode.OK;
-        }
-    }
-
-    public static class NancyMongoDBExtensions
-    {
-        public static Response AsResponse(this IAsyncCursor<BsonDocument> cursor)
-        {
-            var contents = cursor.ToList();
-
-            var json = contents.ToJson(new JsonWriterSettings {OutputMode = JsonOutputMode.Strict});
-            var response = new TextResponse(json, "application/json");
-
-            return response;
+            return docs.ToList().Any();
         }
     }
 }
---
+++ Server/SurveyistServer/Endpoint.cs
@@ -0,0 +1,71 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Driver;
+using Nancy;
+using Nancy.Responses;
+
+namespace SurveyistServer
+{
+    public class Endpoint : NancyModule
+    {
+        public Endpoint()
+        {
+            Options["/{catchAll*}"] = parmeters => { return new Response {StatusCode = HttpStatusCode.Accepted}; };
+
+            After.AddItemToEndOfPipeline(context =>
+            {
+                context.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "POST, GET")
+                    .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+            });
+
+            Get[nameof(PreviousSurveys)] = _ => PreviousSurveys();
+            Get[nameof(ChartTypes)] = _ => ChartTypes();
+            //Get[$"{nameof(PreviousRun)}/{{runId}}"] = parameters => PreviousRun(parameters.runId);
+            //Get[nameof(RuleDefinitions)] = _ => RuleDefinitions();
+            Post[nameof(NewSurvey), true] = async (parameters, token) => NewSurvey();
+        }
+
+        internal DatabaseManager DatabaseManager { get; } = new DatabaseManager();
+
+        private Response PreviousSurveys()
+        {
+            var docCollection = DatabaseManager.GetDocuments("PreviousSurveys");
+
+            return docCollection.AsResponse();
+        }
+
+        private Response ChartTypes()
+        {
+            var docCollection = DatabaseManager.GetDocuments("ChartTypes");
+
+            return docCollection.AsResponse();
+        }
+
+        private Response NewSurvey()
+        {
+            var surveyConfig = Request.Form;
+
+            var newDoc = BsonDocument.Parse(surveyConfig);
+            var SurveyDetails = DatabaseManager.GetCollection("SurveyDetails");
+            SurveyDetails.InsertOne(newDoc);
+
+            return HttpStatusCode.OK;
+        }
+    }
+
+    public static class NancyMongoDBExtensions
+    {
+        public static Response AsResponse(this IAsyncCursor<BsonDocument> cursor)
+        {
+            var contents = cursor.ToList();
+
+            var json = contents.ToJson(new JsonWriterSettings {OutputMode = JsonOutputMode.Strict});
+            var response = new TextResponse(json, "application/json");
+
+            return response;
+        }
+    }
+}
+
+}
